7fa609a63b3488f61fb75c078f47a09007bbc0b4
kinjalrajpal <kinjalrajpal2002@gmail.com>
2025-02-06 11:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	    s = s.trim();
+	
+       let space = s.indexOf(" ");
+
+	   if (space === -1) {
+        return s;
+	  }
+
+	   return s.substring(0, space);
+
 }
 
 // Do not change the code below
